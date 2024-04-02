96b59d07da693978a8c3a2893e746b9a363d0275
J M <wohaste@gmail.com>
2024-04-02 15:01:57 -0700

    exercise 11 - add missing tests

---
+++ solutions/csharp/11_Console_Diagnostics/DiagnosticsTest/TimingTest.cs
@@ -0,0 +1,17 @@
+﻿namespace DiagnosticsTest
+{
+    public class TimingTest
+    {
+        [Fact]
+        public void TimeSortingAlgorithmsTest()
+        {
+            var results = Exercise11.TimeSortingAlgorithms();
+
+            Assert.NotEmpty(results);
+            foreach (var result in results) 
+            {
+                Assert.True(result.MillisecondRunTime > 0);
+            }
+        }
+    }
+}
